46263244650615de6c12e3e83fe574cb69b15951
chandrasekar <61655086+chandrasekar007@users.noreply.github.com>
2025-11-26 13:07:08 +0530

    Add Readme and Screenshots

---
+++ README.md
@@ -0,0 +1,60 @@
+# TOTO APP -Simple Task Manager
+
+Toto App Is a Simple Light Weight and easy-to-use task manager User Friendly Web Application. 
+
+
+# Installation steps 
+   1.Clone the Repository
+
+   git clone https://github.com/chandrasekar007/React-Toto-Application.git
+
+   2.Install Dependencies
+ 
+   npm install
+
+   3.Start the Application
+
+   npm start
+   
+
+# ToTo App Features
+   
+    ToDo App helps users manage daily tasks easily. Below are the main features with clear descriptions:-
+
+ 1. Add Task
+
+    Users can create new tasks by entering a task name . This helps them keep track of things they need to do.
+
+ 2. Edit Task
+ 
+    If a user makes a mistake or wants to update a task, they can edit the existing task anytime.
+
+ 3. Mark Task as Completed
+
+    Once a task is finished, users can mark it as completed. This helps them visually see what’s done and what’s pending.
+
+ 4. Delete Task
+
+    Users can remove tasks they no longer need. This keeps the task list clean .
+
+  
+
+# Screenshots 
+
+ Here is the Toto App Home Screen
+ <p float="left">
+  <img src="./screenshots/Home.png" width="300"/>
+
+  Here is the Toto App Adding Task Screen
+
+  <img src="./screenshots/Add task.png" width="300"/>
+
+  Here is the Toto App Editing Task Screen
+
+ <img src="./screenshots/Edit And Check.png" width="300"/>
+ </p>
+
+
+
+
+
--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
 import './App.css';
 import { useState,useEffect } from 'react';
-
 function App() {
-
-    const [todos,setTodos]=useState(
-      ()=>
-
-      
-        JSON.parse(localStorage.getItem("totos"))||[]
-           
-      );
-
-
+    const [todos,setTodos]=useState(()=> JSON.parse(localStorage.getItem("totos"))||[]); 
      useEffect(()=>
       {
            localStorage.setItem("totos",JSON.stringify(todos))
-           
-
       },[todos]
     )
-      
-
     const [input,setInput]=useState("");
-
     const [edit,setEdit]=useState(null);
-
-
-   
-
     function adddata()
     { 
-      
     if(input ==="")
-
-      { alert("please ender any Task");
+      {
+         alert("please ender any Task");
 
       }
       else
@@ -41,13 +21,10 @@ function App() {
       if(edit!==null)
       {
         const copy=[...todos];
-        copy[edit]=
-        {  ...copy[edit],
-          data:input
-        };
+
+        copy[edit]={  ...copy[edit], data:input };
 
         console.log(todos);
-
 
         setTodos(copy);
 
@@ -62,76 +39,49 @@ function App() {
        setInput("");
 
     }
-
     const edits=(index)=>
     {
        setInput(todos[index].data);
        setEdit(index);
     };
-
-
     const del=(index)=>
     {
       const newtoto=todos.filter((_, i)=> i !==index);
       setTodos(newtoto);
     }
-
-    const [checkbox,setCheckbox]=useState(false);
-    
     function check(index)
     {
-
   const copytodos=[...todos];
   copytodos[index].checked=!copytodos[index].checked;
-   setTodos(copytodos)
+  setTodos(copytodos)
     }  
-
-
     return (
-
     <div className="App">
       <h3 className='h3'>TOTO-APPLICATION</h3>
-
       <div>
-
          <input type="text"  value={input} name="task" onChange={(e)=>setInput(e.target.value)} placeholder='Enter The Task' />
          <button className="abtn" onClick={adddata}>ADD+</button>
-
       </div>
-
       <div className='task-items'>
-
       <ul>
         {
-      
           todos.map(
             (todotask,index)=>
             (
                <li className="card" key={index} style={{}}>
-
                 <input type="checkbox"  onChange={()=>check(index)} checked={todotask.checked} />
-
-               
-
-
                 <h3 id={"ch"}  style={{textDecoration:todotask.checked?"line-through":"none"}}>
                   <p id="textmark" >{todotask.data}</p>
                 </h3>
-
                 <button className="ebtn" onClick={()=>edits(index)}>Edit</button>
-
                 <button className="dbtn" onClick={()=>del(index)}>Delete</button> 
               </li>
             )
           )
         }
       </ul>
-
        </div>
     </div>
-
-
-    
   );
 }
 
